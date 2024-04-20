20a4ec03bdbb7a8aae952048f3291b2303427a11
areenlu <122915481+areenlu@users.noreply.github.com>
2024-04-19 20:55:17 -0700

    Work for labs
    Lab 2-3

--- lab3/movies.js
+++ lab3/movies.js
@@ -1,1 +1,8 @@
 /* This file starts out empty; you will be adding to it in Lab 3 */
+async function populate() {
+    const requestURL = 'https://dsc106.com/resources/data/movies.json';
+    const request = new Request(requestURL);
+    const response = await fetch(request);
+    const movies = await response.json();
+}
+populate();
